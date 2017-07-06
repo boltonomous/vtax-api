5cfdf9214740b15b7f0ea8bce4c3f285a978e938
Clifton, Ashley <aclifton@hearst.com>
2017-07-06 14:28:22 -0400

    VTAX-21 - Refactor user.js to be easier to test

--- common/models/user.js
+++ common/models/user.js
@@ -44,7 +44,23 @@ module.exports = function(User) {
       return;
     }
 
-    User.getAccessTokenFromRover(null, email, password, callback);
+    // @todo handle refreshing token
+    var type = null;
+    var refreshToken = '';
+
+    var options = (type === 'refresh')
+      ? User.refreshTokenOptions(refreshToken)
+      : User.newAccessTokenOptions(email, password);
+
+    var params = {
+      options: options,
+      email: email,
+      password: password,
+      lbCallback: callback,
+      callback: User.retrieveUserInfo
+    };
+
+    User.getAccessTokenFromRover(params);
   };
 
   /**
@@ -52,42 +68,16 @@ module.exports = function(User) {
    * and then takes that user token and retrieves user information based
    * on the email and password passed into the function.
    *
-   * @param type: string
-   * @param email: string
-   * @param password: string
-   * @param callback: function
+   * params {
+   *  type: string
+   *  email: string
+   *  password: string
+   *  lbCallback: function
+   *  callback: function
+   * }
    */
-  User.getAccessTokenFromRover = function(type, email, password, callback) {
-    var newAccessTokenOptions = {
-      url: authDomain + '/openid/token',
-      form: {
-        client_id: clientId,
-        timestamp: Math.floor(Date.now() / 1000),
-        email: email,
-        password: password,
-        grant_type: 'password',
-        scope: 'openid user roles profiles'
-      }
-    };
-
-    // @todo Add functionality to get refresh token before this point
-    var refreshTokenOptions = {
-      url: authDomain + '/openid/token',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      form: {
-        refresh_token: '', // refresh token goes here
-        client_id: clientId,
-        grant_type: 'refresh_token'
-      }
-    };
-
-    var options = (type === 'refresh')
-      ? refreshTokenOptions
-      : newAccessTokenOptions;
-
-    request.post(newAccessTokenOptions, function (error, response, body) {
+  User.getAccessTokenFromRover = function(params) {
+    request.post(params.options, function (error, response, body) {
       User.consoleInfo(error, response, body);
 
       var data = JSON.parse(body);
@@ -95,11 +85,11 @@ module.exports = function(User) {
 
       if (!accessToken) {
         // send 'status: denied' response object to loopback
-        callback(null, {status: 'denied'});
+        params.lbCallback(null, {status: 'denied'});
         return;
       }
 
-      User.retrieveUserInfo(accessToken, callback);
+      params.callback(accessToken, params.lbCallback);
     });
   };
 
@@ -111,12 +101,7 @@ module.exports = function(User) {
    * @param callback: function
    */
   User.retrieveUserInfo = function(accessToken, callback) {
-    var options = {
-      url: authDomain + '/openid/userinfo/',
-      headers: {
-        Authorization: 'Bearer ' + accessToken
-      }
-    };
+    var options = User.userInfoOptions(accessToken);
 
     request.post(options, function (error, response, body) {
       User.consoleInfo(error, response, body);
@@ -145,5 +130,54 @@ module.exports = function(User) {
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
     console.log('body:', body);
+  };
+
+  /**
+   * Returns Options for making an api POST request
+   * to return a brand new access token.
+   */
+  User.newAccessTokenOptions = function (email, password) {
+    return {
+      url: authDomain + '/openid/token',
+      form: {
+        client_id: clientId,
+        timestamp: Math.floor(Date.now() / 1000),
+        email: email,
+        password: password,
+        grant_type: 'password',
+        scope: 'openid user roles profiles'
+      }
+    }
+  };
+
+  /**
+   * Takes in refresh token (string) and returns options for
+   * making an api POST request to return a refreshed access token.
+   */
+  User.refreshTokenOptions = function (refreshToken) {
+    return {
+      url: authDomain + '/openid/token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      form: {
+        refresh_token: refreshToken,
+        client_id: clientId,
+        grant_type: 'refresh_token'
+      }
+    }
+  };
+
+  /**
+   * Takes in access token (string) and returns options for
+   * making an api POST request to return user info.
+   */
+  User.userInfoOptions = function (accessToken) {
+    return {
+      url: authDomain + '/openid/userinfo/',
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    };
   };
 };
